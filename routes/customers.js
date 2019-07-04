0cff99ccfbfa6361d38b57e263da7c7944836006
Joshua Olowogbayi <olowogbayigbenga@gmail.com>
2019-07-04 09:38:49 +0100

    finished customer endpoints

--- routes/customers.js
+++ routes/customers.js
@@ -1,12 +1,12 @@
 const Customer = require("../models/Customer");
 const errors = require("restify-errors");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 // create application/json parser
-// const jsonParser = bodyParser.json();
+const jsonParser = bodyParser.json();
 
 module.exports = app => {
+    // get all customer
     app.get('/customers', async (req, res) => {
-        return res.send('hello');
         try {
             const customers = await Customer.find({});
             return res.send(customers).status(200);
@@ -15,23 +15,66 @@ module.exports = app => {
         }
 
     })
-    // app.post('/customers', async (req, res) => {
-    //     const {
-    //         name,
-    //         email,
-    //         balance
-    //     } = req.body;
-
-    //     try {
-    //         const customer = new customer({
-    //             name,
-    //             email,
-    //             balance
-    //         })
-    //         const newCusomer = await customer.save();
-    //         return res.send
-    //     } catch (err) {
-    //         return res.send(new errors.BadRequestError(err));
-    //     }
-    // })
+    // Add new Customer
+    app.post('/customers', jsonParser, async (req, res) => {
+        if (!req.is('application/json')) {
+            return res.send(new errors.InvalidContentError("Expect application/json"));
+        }
+        const {
+            name,
+            email,
+            balance
+        } = req.body;
+        try {
+            const customer = new Customer({
+                name,
+                email,
+                balance
+            })
+            const newCustomer = await customer.save();
+            return res.json({
+                success: true
+            }).status(201);
+        } catch (err) {
+            return res.status(500).send(new errors.InternalError(err.message));
+        }
+    });
+    // get single user
+    app.get('/customers/:id', async (req, res) => {
+        try {
+            const customer = await Customer.findById(req.params.id);
+            return res.send(customer).status(200);
+        } catch (err) {
+            return res.send(new errors.ResourceNotFoundError(`There is no customer with ${req.params.id}`))
+        }
+    });
+    // update of customer
+    app.put('/customers/:id', jsonParser, async (req, res) => {
+        if (!req.is('application/json')) {
+            return res.send(new errors.InvalidContentError("Expect application/json"));
+        }
+        try {
+            const customer = await Customer.findOneAndUpdate({
+                _id: req.params.id
+            }, req.body);
+            return res.json({
+                success: true
+            }).status(201);
+        } catch (err) {
+            return res.status(500).send(new errors.InternalError(err.message));
+        }
+    });
+    // delete customer
+    app.delete('/customers/:id', async (req, res) => {
+        try {
+            const customer = await Customer.findOneAndRemove({
+                _id: req.params.id
+            });
+            return res.json({
+                success: true
+            }).status(201);
+        } catch (err) {
+            return res.status(500).send(new errors.InternalError(err.message));
+        }
+    })
 }
